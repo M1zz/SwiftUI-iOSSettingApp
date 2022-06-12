b0047ab9bdfb81bbbe616bb0752aab630592106b
hyunho <mizzking75@gmail.com>
2022-06-12 22:53:07 +0900

    Initial Commit

---
+++ SampleLeeoSettingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleLeeoSettingApp
+//
+//  Created by hyunho lee on 2022/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleLeeoSettingApp/SampleLeeoSettingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLeeoSettingAppApp.swift
+//  SampleLeeoSettingApp
+//
+//  Created by hyunho lee on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct SampleLeeoSettingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
